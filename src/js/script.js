276d912a70562bb3d2b83db12c776e2ff14b79ff
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-12-15 15:16:15 +0100

    refactor initAction to make it more efficent and change event on heart img

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const books = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const booksList = document.querySelector('.books-list');
+const favoriteBooks = [];
 
 const renderFunction = () => {
   for (let book of dataSource.books) {
@@ -11,27 +12,21 @@ const renderFunction = () => {
 
 renderFunction();
 
-const favoriteBooks = [];
-
 const initActions = () => {
-  const booksImages = booksList.querySelectorAll('.book__image');
-  booksImages.forEach(item => {
-    item.addEventListener('dblclick', event => {
-      event.preventDefault();
-      const id = item.getAttribute('data-id');
-      const index = favoriteBooks.indexOf(id);
 
-      if (item.classList.contains('favorite')) {
-        item.classList.remove('favorite');
-        favoriteBooks.splice(index, 1);
-      } else {
-        item.classList.add('favorite');
-        favoriteBooks.push(id);
-      }
-    });
+  booksList.addEventListener('dblclick', event => {
+    event.preventDefault();
+    const id = event.target.getAttribute('data-id');
+    const index = favoriteBooks.indexOf(id);
 
+    if (event.target.classList.contains('favorite')) {
+      event.target.classList.remove('favorite');
+      favoriteBooks.splice(index, 1);
+    } else {
+      event.target.classList.add('favorite');
+      favoriteBooks.push(id);
+    }
   });
-
 };
 
 initActions();

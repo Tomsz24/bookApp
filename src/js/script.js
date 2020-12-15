1f60a0b8f300a95b163e6d0195135f437a09f8d3
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-12-15 14:30:26 +0100

    add listeneres for images and toggle id and class favorite in them

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,28 @@ const renderFunction = () => {
 };
 
 renderFunction();
+
+const favoriteBooks = [];
+
+const initActions = () => {
+  const booksImages = booksList.querySelectorAll('.book__image');
+  booksImages.forEach(item => {
+    item.addEventListener('dblclick', event => {
+      event.preventDefault();
+      const id = item.getAttribute('data-id');
+      const index = favoriteBooks.indexOf(id);
+
+      if (item.classList.contains('favorite')) {
+        item.classList.remove('favorite');
+        favoriteBooks.splice(index, 1);
+      } else {
+        item.classList.add('favorite');
+        favoriteBooks.push(id);
+      }
+    });
+
+  });
+
+};
+
+initActions();
